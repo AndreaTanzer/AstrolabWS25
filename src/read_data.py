96ab408eedb9b73c9999a25913c06ab1609a9cc4
Andrea Tanzer <andrea.tanzer@gmx.at>
2026-01-20 22:59:49 +0100

    added safe path for mac and regex for FITs

--- src/read_data.py
+++ src/read_data.py
@@ -7,24 +7,31 @@ Created on Tue Jan 20 17:14:45 2026
 
 import glob
 from astropy.io import fits
+import os
 
 
 def read_folder(path):
+    print(f"Reading now: {path}")
+
     hdrs = []
     datas = []
-    for fname in glob.glob(path+"*.fit"):
+    files = glob.glob(os.path.join(path, "*.[Ff][Ii][Tt]*"))
+
+    for fname in files:
         with fits.open(fname) as hdul:
             hdrs.append(hdul[0].header)
             datas.append(hdul[0].data)
+    print(f"Finished reading {len(files)} files from {path}")
     return hdrs, datas
 
-def read(directory="../20251104_lab/"):
-    print("Hi")
-    hdrs_bias, bias = read_folder(directory+"Bias/")
-    hdrs_flat, flats = read_folder(directory+"Flats/")
-    hdrs_sci, scis = read_folder(directory+"Science/")
+def read(directory="data/20251104_lab/"):
+    directory = os.path.abspath(os.path.expanduser(directory))
+    hdrs_bias, bias = read_folder(os.path.join(directory, "Bias"))
+    hdrs_flat, flats = read_folder(os.path.join(directory, "Flats"))
+    hdrs_sci, scis = read_folder(os.path.join(directory, "Science"))
+
     hdr = {"sci":hdrs_sci, "bias": hdrs_bias, "flat": hdrs_flat}
-    data = {"sci":scis, "bias": hdrs_bias, "flat": hdrs_flat}
+    data = {"sci":scis, "bias": bias, "flat": flats}
     return hdr, data
 
 
